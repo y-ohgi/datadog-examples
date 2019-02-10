3aa11586febde87eea228086fa75e92d5dc8e647
y-ohgi <ohgi.211@gmail.com>
2019-02-10 09:21:30 +0900

    Add proxy path

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const sassMiddleware = require('node-sass-middleware')
+var proxy = require('express-http-proxy');
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+app.use('/proxy', proxy('jsonplaceholder.typicode.com'))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
